3ae363e5923338b3cad63d514f100d7a4068e1c4
PrIvATe <kpackalv@yahoo.com>
2020-06-02 20:32:06 +0300

    Commit 1 02/06/2020

--- src/com/Artur/Main.java
+++ src/com/Artur/Main.java
@@ -1,5 +1,8 @@
 package com.Artur;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,27 +12,42 @@ public class Main {
 
         Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
         adelaideCrows.addPlayer(joe);
+//        adelaideCrows.addPlayer(pat);
+//        adelaideCrows.addPlayer(beckham);
 
         System.out.println(adelaideCrows.numPlayers());
 
-        Team<BaseballPlayer> baseBallTeam = new Team<>("Chicago Cubs");
-        baseBallTeam.addPlayer(pat);
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(pat);
 
-        Team<SoccerPlayer> brokenTeam = new Team<>("this won;t work");
+        Team<SoccerPlayer> brokenTeam = new Team<>("this won't work");
         brokenTeam.addPlayer(beckham);
 
-        Team<FootballPlayer> melbourn = new Team<>("Melnbourn");
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
         FootballPlayer banks = new FootballPlayer("Gordon");
-        melbourn.addPlayer(banks);
+        melbourne.addPlayer(banks);
 
-        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
-        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+        Team<FootballPlayer> hawthorn= new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle= new Team<>("Fremantle");
 
         hawthorn.matchResult(fremantle, 1, 0);
         hawthorn.matchResult(adelaideCrows, 3, 8);
 
         adelaideCrows.matchResult(fremantle, 2, 1);
-        adelaideCrows.matchResult(baseBallTeam, 1, 1);
+//        adelaideCrows.matchResult(baseballTeam, 1, 1);
 
+        System.out.println("Rankings");
+        System.out.println(adelaideCrows.getName() + ": " + adelaideCrows.ranking());
+        System.out.println(melbourne.getName() + ": " + melbourne.ranking());
+        System.out.println(fremantle.getName() + ": " + fremantle.ranking());
+        System.out.println(hawthorn.getName() + ": " + hawthorn.ranking());
+
+        System.out.println(adelaideCrows.compareTo(melbourne));
+        System.out.println(adelaideCrows.compareTo(hawthorn));
+        System.out.println(hawthorn.compareTo(adelaideCrows));
+        System.out.println(melbourne.compareTo(fremantle));
+
+        ArrayList<Team> teams;
+        Collections.sort(teams);
     }
 }
